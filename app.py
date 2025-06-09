97915a91624020643d978948fac072aee56d4790
João Pedro Pimentel <137961559+joaoppimentel@users.noreply.github.com>
2025-06-09 11:33:41 -0300

    Add files via upload

---
+++ app.py
@@ -0,0 +1,9 @@
+import streamlit as st
+from utils.database import create_tables, feed_tables
+
+def main():
+    st.set_page_config(page_title="Sistema Academia", layout="wide")
+
+
+if __name__ == "__main__":
+    main()
