1f0aece33f67755cb3982ae0e1cf6616744ba10b
João Pedro <joaopedroap50@gmail.com>
2025-06-09 12:28:29 -0300

    implement db

--- app.py
+++ app.py
@@ -1,8 +1,10 @@
 import streamlit as st
-from utils.database import create_tables, feed_tables
+import pandas as pd
 
 def main():
     st.set_page_config(page_title="Sistema Academia", layout="wide")
+
+    df = pd.read_csv('./data/ds_salaries.csv')
 
 
 if __name__ == "__main__":
