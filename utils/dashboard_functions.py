8af3556d983f478f0c6c87748b4ca9d1112d7478
Felipe Olivato Coimbra <felipe.olicoimbra@gmail.com>
2025-06-12 11:24:22 -0300

    refactor: Changes to layout

--- utils/dashboard_functions.py
+++ utils/dashboard_functions.py
@@ -32,13 +32,15 @@ def return_df():
 
 def highest_average_salary(df, year, column):
     
+    column = column.lower().replace(' ', '_')
+
     if year == 'All':
         column_average = df.groupby([column]).aggregate(average_salary=('salary_in_usd','mean')).reset_index()
         highest_column = column_average.iloc[column_average['average_salary'].idxmax()]
         salary = round(highest_column['average_salary'], 2)
         column_name = highest_column[column]
         highest_average_metric = st.metric(
-            label=f'Highest average salary {column.replace('_', ' ')}',
+            label=f'Highest average salary {column.replace('_', ' ')} | {year}',
             value= f'{salary}$ | {column_name}'
         )  
     elif (year-1) in df['work_year'].values:
@@ -53,9 +55,9 @@ def highest_average_salary(df, year, column):
         column_current_year = highest_column_current_year[column]
         column_last_year = highest_column_last_year[column]
         highest_average_metric = st.metric(
-            label=f'Highest average salary {column.replace('_', ' ')}',
+            label=f'Highest average salary {column.replace('_', ' ')} | {year}',
             value=f'{salary_current_year}$ | {column_current_year}',
-            delta=f'{delta_percentage}% | {column_last_year}'
+            delta=f'{delta_percentage}% | {column_last_year} | {year-1}'
         )
     else:
         column_average = df.loc[df['work_year'] == year].groupby([column]).aggregate(average_salary=('salary_in_usd','mean')).reset_index()
@@ -63,7 +65,7 @@ def highest_average_salary(df, year, column):
         salary = round(highest_column['average_salary'], 2)
         column_name = highest_column[column]
         highest_average_metric = st.metric(
-            label=f'Highest average salary {column.replace('_', ' ')}',
+            label=f'Highest average salary {column.replace('_', ' ')} | {year}',
             value= f'{salary}$ | {column_name}'
         )
 
